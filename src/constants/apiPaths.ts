a0c6869ac0d3140409a966b5aed561b3bf1ef7eb
Anton Kuzmenko <anton_kuzmenko@epam.com>
2021-07-03 14:01:29 +0300

    Add cart service path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://ng8k6rhfp1.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://xnj469am26.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://kzanton-cart-api-develop.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
